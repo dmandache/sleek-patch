4b27d5b0fad0940ebc09388acf9bd5da9703205d
dmandache <diana.mandache00@gmail.com>
2022-06-07 12:16:16 +0300

    structure change

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import setuptools
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setuptools.setup(name='sleek',
-version='0.1',
-description='Texture aware patchification using SLIC superpixel algoithm',
-url='https://github.com/dmandache/sleek-patch',
-author='diana',
-install_requires=['scikit-image', 'numpy'],
-author_email='',
-packages=setuptools.find_packages(),
-zip_safe=False)
+    version='0.1',
+    description='Texture aware patchification using SLIC superpixel algoithm',
+    url='https://github.com/dmandache/sleek-patch',
+    author='diana',
+    install_requires=['scikit-image', 'numpy'],
+    author_email='',
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    zip_safe=False)
